01a7332eb52c4acde64be4c3a11f5352c786b29b
tyler jones <rteyl1690@gmail.com>
2020-02-26 23:16:11 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,10 @@
 class Book
+  attr_accessor :title, :author, :page_count, :genre
+
   def initialize(title)
     @title = title
   end
-  attr_accessor :title, :author, :page_count, :genre
+
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
